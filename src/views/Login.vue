d40320dc577d08b2a27aa53160fa23cafdc07351
mengchao <695263117@qq.com>
2018-09-06 20:45:46 +0800

    登录页

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="login-wr">
-        <el-form label-position="top" label-width="80px" class="login-form">
+        <el-form label-position="top" label-width="80px" class="login-form" :model="dormData">
             <h2>用户登录</h2>
             <el-form-item label="用户名">
-                <el-input ></el-input>
+                <el-input v-model="formData.username"></el-input>
             </el-form-item>
             <el-form-item label="密码">
-                <el-input  type="password"></el-input>
+                <el-input  type="password" v-model="formData.password"></el-input>
             </el-form-item>
             <el-form-item>    
-                <el-button class="btn" type="primary">登录</el-button>
+                <el-button @click="handleLogin" class="btn" type="primary">登录</el-button>
            </el-form-item>
         </el-form>
     </div>
@@ -17,8 +17,35 @@
 
 <script>
 export default {
-    
-}
+    data () {
+        return {
+            formData:{
+                username:'',
+                password:''
+            }
+        }
+    },
+    methods: {
+        handleLogin() {
+            this.$http
+            .post('login',this.formData)
+            .then((response) => {
+                // response.data.meta.status
+                const { meta: {msg,status}} = response.data;
+                if(status === 200) {
+                    this.$message.success(msg);
+                    // 记录token
+                    sessionStorage.setItem('token',response.data.data.token)
+                }else {
+                    this.$message.error(msg);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        }
+    }
+};
 </script>
 
 <style>
